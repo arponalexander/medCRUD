69c7a1a03a08f6616ecf5d95e73f80701ca5cd89
ADA <arpon.alexander@gmail.com>
2020-06-01 15:50:04 +0800

    4th commit

--- medicalcrud/urls.py
+++ medicalcrud/urls.py
@@ -3,9 +3,9 @@ from . import views
 from rest_framework import routers
 
 routers = routers.DefaultRouter()
-routers.register('Medication_R01', views.MedicationR01View)
-routers.register('Medication_R02', views.MedicationR02View)
-routers.register('Medication_B00', views.MedicationB00View)
+routers.register('medication_r01', views.MedicationR01View)
+routers.register('medication_r02', views.MedicationR02View)
+routers.register('medication_b00', views.MedicationB00View)
 
 urlpatterns = [
     path('', include(routers.urls))
